63120ebb6bbaeb5c5c30c5f76e05ad1de7829259
Bobby Connolly <connollythomas@hotmail.com>
2022-12-02 17:44:55 -0500

    init

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+const data = [
+    {
+        id: 1,
+        name: "John Doe",
+        age: 32,
+    },
+    {
+        id: 2,
+        name: "Jane Doe",
+        age: 30,
+    },
+    {
+        id: 3,
+        name: "John Smith",
+        age: 25,
+    },
+];
+
+// This route gets *ALL* the users
+app.get("/api/users", (req, res) => {
+    res.json(data);
+});
+
+// Add a new route to get a *SINGLE* user (you can use either path param or query param)
+// /api/users/1      <-- path param (req.params.id)
+// /api/users?id=1   <-- query param (req.query.id)
+
+app.listen(3000, () => {
+    console.log("Example app listening on port 3000!");
+});
