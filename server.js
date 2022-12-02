b2371aba6975632507765f5be154549a46068301
Bobby Connolly <bobbyconnolly@bobbys-mbp.lan>
2022-12-02 18:11:14 -0500

    clarification

--- server.js
+++ server.js
@@ -3,34 +3,34 @@ const express = require("express");
 const app = express();
 
 const data = [
-    {
-        id: 1,
-        name: "John Doe",
-        age: 32,
-    },
-    {
-        id: 2,
-        name: "Jane Doe",
-        age: 30,
-    },
-    {
-        id: 3,
-        name: "John Smith",
-        age: 25,
-    },
+  {
+    id: 1,
+    name: "John Doe",
+    age: 32,
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 30,
+  },
+  {
+    id: 3,
+    name: "John Smith",
+    age: 25,
+  },
 ];
 
 // This route gets *ALL* the users
 app.get("/api/users", (req, res) => {
-    res.json(data);
+  res.json(data);
 });
 
 // Add a new route to get a *SINGLE* user (you can use either path param or query param)
 // /api/users/1      <-- path param (req.params.id)
-// /api/users?id=1   <-- query param (req.query.id)
+// /api/users?id=1   <-- query param (req.query.id) If you go with query param, just modify the existing endpoint above instead of creating a new endpoint
 
 // BONUS QUESTION - Add routes to implement all the CRUD operations (POST, PUT, DELETE)
 
 app.listen(3000, () => {
-    console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port 3000!");
 });
