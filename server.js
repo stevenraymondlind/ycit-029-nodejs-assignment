5f8aef1a6ead97c978629a462443ef7c9d3d4024
Steven Lind <stevenraymondlind@gmail.com>
2022-12-14 13:10:04 -0500

    homework-ycit29

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 
 const app = express();
@@ -26,11 +27,34 @@ app.get("/api/users", (req, res) => {
 });
 
 // Add a new route to get a *SINGLE* user (you can use either path param or query param)
+
+
+
 // /api/users/1      <-- path param (req.params.id)
 // /api/users?id=1   <-- query param (req.query.id) If you go with query param, just modify the existing endpoint above instead of creating a new endpoint
 
 // BONUS QUESTION - Add routes to implement all the CRUD operations (POST, PUT, DELETE)
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+
+app.get("/api/users/:id", (req, res) => {
+  let user = req.params.id - 1
+  res.json(data[user])
+});
+app.post("/api/users/:id", (req, res) => {
+  let id = req.params.id - 1
+  res.json(data[id])
+});
+app.put("/api/users/:id", (req, res) => {
+  let id = req.params.id - 1
+  res.json(data[id])
+});
+app.delete("/api/users/:id", (req, res) => {
+  let id = req.params.id - 1
+  res.json(data[id])
+});
+
+
+
+app.listen(3030, () => {
+  console.log("Example app listening on port 3030!");
 });
